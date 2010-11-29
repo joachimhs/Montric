c099501d691d66e0e795a4e84cc73e28b4ae8376
Joachim Haagen Skeie <joachim@haagen.name>
2010-11-29 23:07:21 +0000

    Changed design of the Instrumentation Group panel (SproutCore).

--- EurekaJView/apps/eureka_j_view/controllers/instrumentation_group.js
+++ EurekaJView/apps/eureka_j_view/controllers/instrumentation_group.js
@@ -18,8 +18,8 @@ EurekaJView.instrumentationGroupController = SC.ObjectController.create(
     showPanelPane: function() {
         var pane = SC.PanelPane.create({
             layout: {
-                left: 75,
-				right: 75,
+                centerX: 0,
+				width: 870,
 				top: 75,
 				bottom: 75,
             },
@@ -38,7 +38,7 @@ EurekaJView.instrumentationGroupController = SC.ObjectController.create(
 					anchorLocation: SC.ANCHOR_TOP,
 					
 					labelView: SC.LabelView.extend({
-	                    layout: {centerY: 0, centerX:0, centerY: 0, height: 40, top: 10, width: 400 },
+	                    layout: {centerY: 0, centerX:0, height: 40, top: 10, width: 250 },
 						controlSize: SC.LARGE_CONTROL_SIZE,
 						fontWeight: SC.BOLD_WEIGHT,
 	                    value: 'Instrumentation Groups'
@@ -46,7 +46,8 @@ EurekaJView.instrumentationGroupController = SC.ObjectController.create(
 				}),
 				
 				mainContentView: SC.View.extend({
-					childViews: 'instrumentationGroupLeft instrumentationGroupLeftScrollView hideInstrumentationGroupButtonView'.w(),
+					childViews: ['instrumentationGroupLeft','instrumentationGroupLeftScrollView','hideInstrumentationGroupButtonView','groupNameLabelView','groupNameTextFieldView',
+						'sourceGroupLabelView', 'sourceGroupSelectFieldView', 'selectChartsLabelView', 'selectedChartsScrollView'],
 					layout: {top: 40, bottom: 0, right: 0, left: 0},
 					
 					instrumentationGroupLeft: SC.View.design({
@@ -81,6 +82,61 @@ EurekaJView.instrumentationGroupController = SC.ObjectController.create(
 							backgroundColor: '#F0F8FF'
 						}),
 					}),
+					
+					groupNameLabelView: SC.LabelView.extend({
+	                    layout: {left: 320, width: 180, height: 25, top: 20},
+						controlSize: SC.NORMAL_CONTROL_SIZE,
+	                    value: 'Instrumentation Group Name:'
+	                }),
+	
+					groupNameTextFieldView: SC.TextFieldView.design({
+						layout: {top: 15, height: 25, left: 515, width: 350},
+					}),
+					
+					sourceGroupLabelView: SC.LabelView.extend({
+	                    layout: {left: 320, width: 180, height: 25, top: 55},
+						controlSize: SC.NORMAL_CONTROL_SIZE,
+	                    value: 'Instrumentation Group Source:'
+	                }),
+	
+					sourceGroupSelectFieldView: SC.SelectFieldView.design({
+						layout: {top: 55, height: 25, left: 515, width: 350},
+
+					  objects: [
+					    {name:'For 3 Seconds', value:'3seconds'},
+					    {name:'Until the browser is closed', value:'closeBrowser'},
+					    {name:'For 1 year', value:'1year'}
+					    ],
+					  nameKey: 'name',
+					  valueKey: 'value',
+
+					  acceptsFirstResponder: function() {
+					    return this.get('isEnabled');
+					  }.property('isEnabled'),
+
+					  //valueBinding: 'LoginLogoutSample.loginPageController.rememberMe'
+					}),
+					
+					selectChartsLabelView: SC.LabelView.extend({
+	                    layout: {left: 320, width: 180, height: 25, top: 85},
+						controlSize: SC.NORMAL_CONTROL_SIZE,
+	                    value: 'Select Charts:'
+	                }),
+	
+					selectedChartsScrollView: SC.ScrollView.design({
+						layout: {top: 85, bottom: 45, left: 515, width: 350},
+						hasHorizontalScroller: YES,
+						hasVerticalScroller: YES,
+
+						contentView: SC.ListView.design({
+							backgroundColor: '#F0F8FF',
+							contentValueKey: "name",
+							//contentBinding: 'EurekaJView.instrumentationChartController.arrangedObjects',
+							//selectionBinding: 'EurekaJView.instrumentationChartController.selection'
+
+						}),
+					}),
+					
 					
 					hideInstrumentationGroupButtonView: SC.ButtonView.extend({
 	                    layout: {
