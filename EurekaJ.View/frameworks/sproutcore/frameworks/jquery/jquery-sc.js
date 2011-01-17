90669274523a38ff58f4ac2170eead28d1f099c4
Joachim Haagen Skeie <joachim@haagen.name>
2011-01-17 23:07:48 +0100

    Removing Sproutcore before adding as a submodule

--- EurekaJ.View/frameworks/sproutcore/frameworks/jquery/jquery-sc.js
+++
@@ -1,7 +0,0 @@
-// sc_require("jquery-buffer");
-jQuery.Buffer.scheduleFlushing = function() {
-  SC.RunLoop.currentRunLoop.invokeOnce(function() {
-    jQuery.Buffer.flush()
-  });
-  this.flushingScheduled = true;
-};
