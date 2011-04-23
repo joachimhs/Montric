af831fb10a8e51374365fd87340fedc24c266830
Joachim Haagen Skeie <joachim@haagen.name>
2011-04-23 21:48:26 +0200

    Removing Garcon, so that it can be added as a submodule instead.

--- EurekaJ.View/garcon/lib/garçon.js
+++
@@ -1,7 +0,0 @@
-var
-  self = this;
-
-self.Server = require('./server').Server;
-self.File = require('./file').File;
-self.Framework = require('./framework').Framework;
-self.sharedHandlers = require('./handlers').sharedHandlers;
