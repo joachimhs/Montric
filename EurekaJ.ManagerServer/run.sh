1c4ee3574fcb5c51f36c6dc9417a6cb1b666df48
Joachim H. Skeie <jhs@jhs.local>
2011-05-20 14:43:22 +0200

    Adding sample scripts.

---
+++ EurekaJ.ManagerServer/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This script is used to run EurekaJ on the EC2 instance: http://haagen.name/2011/05/17/Moving_the_profiling_into_the_cloud.html
+
+workingdir=/home/ec2-user/eurekaj
+app=eurekaJ.managerServer
+server=eurekaJ.ManagerServer
+execClass=org.eurekaJ.managerServer.JettyServer
+JAVA_OPTS="-Xmx1512m -Xms768m -Djava.io.tmpdir=/tmp -Dbtrace.agent=EurekaJAgent -Dlog4j.configuration=file:/home/ec2-user/eurekaj/log4j.xml -javaagent:/home/ec2-user/btrace/1.2/btrace-agent.jar=,scriptdir=/home/ec2-user/btrace/scripts,stdout=false,fileRollMilliseconds=7500"
+
+#####
+
+currdir=`pwd`
+cd $workingdir
+
+rm -rf $app
+unzip $app.zip > /dev/null
+#cd $currdir
+#sh $app/bin/$server > /dev/null 2>&1 &
+classpathVar=$(find $workingdir/$app -name '*.jar' -exec printf :{} ';')$(find $workingdir/$app -name '*.war' -exec printf :{} ';')
+
+#Start EurekaJ Manager
+nohup java $JAVA_OPTS -classpath $classpathVar -Dbasedir=$workingdir/$app $execClass > /dev/null 2>&1 &
---
+++ EurekaJ.Proxy/run..sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#This script is used to run EurekaJ Proxy on EC2: http://haagen.name/2011/05/17/Moving_the_profiling_into_the_cloud.html
+
+workingdir=/home/ec2-user/eurekajproxy
+jarFile=eurekaJ.Proxy-0.1-jar-with-dependencies.jar
+JAVA_OPTS="-Xmx64m -Xms32m"
+
+#####
+
+currdir=`pwd`
+cd $workingdir
+
+nohup java -jar $workingdir/$jarFile /home/ec2-user/btrace/scripts http://localhost:8080/liveStatistics > output.txt &
