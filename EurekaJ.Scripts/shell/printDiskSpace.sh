9c7f715346a7c3130c30252357c4e646dae4ac3b
Joachim Haagen Skeie <joachim@haagen.name>
2011-08-31 07:37:51 -0700

    Adding shell script to print out remaining disk space

---
+++ EurekaJ.Scripts/shell/printDiskSpace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+timeperiod=$(date +%s)
+let timeperiod+=0
+let timeperiodstr=timeperiod/15
+let timeperiodstr=timeperiodstr*15000
+
+eval percentageArray=(`df -h | grep -vE "^Filesystem|shm|boot" | awk '{ print ($6 ":Percent_Used___" ":Percent_Used___" +$5) }'`)
+
+
+for percent in "${percentageArray[@]}"
+do
+        match="___"
+        repl=";"
+        echo "[Value;eurekaj-ec2.haagen.name;Disk;${percent//$match/$repl};n;value;$timeperiodstr]"
+done
