ead021ffebb4a9992ad636029060e265e8817805
Joachim Haagen Skeie <joachim@haagen.name>
2011-09-08 09:53:10 +0200

    Changing version to 1.0

---
+++ EurekaJ.Scripts/setup/copyEurekaJ.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd eurekaj
+curl -o eurekaJ.managerServer.zip http://nightly.haagen.name/EurekaJ/latest/eurekaJ.ManagerServer-1.0.zip
+cd ..
--- EurekaJ.Scripts/setup/setupEurekaJProxy.sh
+++ EurekaJ.Scripts/setup/setupEurekaJProxy.sh
@@ -2,7 +2,7 @@
 
 mkdir eurekajproxy
 cd eurekajproxy
-curl -o eurekaJ.Proxy.jar http://nightly.haagen.name/EurekaJ/latest/eurekaJ.Proxy-1.0.RC1-jar-with-dependencies.jar
+curl -o eurekaJ.Proxy.jar http://nightly.haagen.name/EurekaJ/latest/eurekaJ.Proxy-1.0-jar-with-dependencies.jar
 curl -o config.properties https://raw.github.com/joachimhs/EurekaJ/master/EurekaJ.Proxy/config.properties
-curl -o run.sh https://raw.github.com/joachimhs/EurekaJ/master/EurekaJ.Proxy/run..sh
+curl -o run.sh https://raw.github.com/joachimhs/EurekaJ/master/EurekaJ.Proxy/run.sh
 chmod 755 run.sh
