90da2a02f5ce79f212d0c4ade3de689a9776b671
Joachim Haagen Skeie <joachim@haagen.name>
2011-10-10 01:37:15 +0200

    * Alert plugin Architecture Phase 1 * Alerts added to top menu * Jetty 8.0.2 * Changed from System.out to Log4J throughout

--- EurekaJ.Api/src/main/java/org/eurekaj/api/dao/AlertDao.java
+++ EurekaJ.Api/src/main/java/org/eurekaj/api/dao/AlertDao.java
@@ -23,13 +23,6 @@ import org.eurekaj.api.datatypes.TriggeredAlert;
 
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA.
- * User: jhs
- * Date: 5/5/11
- * Time: 9:34 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface AlertDao {
     public void persistAlert(Alert alert);
 
